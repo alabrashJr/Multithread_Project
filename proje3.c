7657c06258f1bc0731878be6d295faa0480543e9
abode965 <you@example.com>
2017-12-31 00:18:50 +0300

    edit the shave chair funciton to mutex insteade of semaphore

--- proje3.c
+++ proje3.c
@@ -15,7 +15,7 @@ sem_t bekleme_Koltugu1_Mutex;
 sem_t bekleme_Koltugu2_Mutex;
 sem_t bekleme_Koltugu3_Mutex;
 pthread_t trasid;
-sem_t tras_Koltugu_Mutex;
+pthread_mutex_t tras_Koltugu_Mutex;
 sem_t semaMutex; // berbar salonu
 int mevcutSayisi=0;
 Koltuk Beklemekoltugu[3];
@@ -30,13 +30,14 @@ void *BeklemeKoltugu1 (void * parameter ){
      Koltuk * offset= ( Koltuk*)parameter;
     printf("\tK1<%d>:T%d\n",clock(),offset->thread_no);
    if(trasK.dolumu==1)
-    printf("girildi");
+    printf("girildi\n");wait(TrasKoltugu);
     //printf("40.satir\n");
     offset->dolumu=0;
     trasK.thread_no=offset->thread_no;
     pthread_create(&trasid,NULL,TrasKoltugu,&trasK);
     mevcutSayisi--;
     pthread_join(trasid,0);
+    //pthread_mutex_lock(&tras_Koltugu_Mutex);
      sem_post(&bekleme_Koltugu1_Mutex);                           //semaphore kritik bolgesi Bitisi
 
 }
@@ -46,12 +47,13 @@ void *BeklemeKoltugu2(void * parameter ){
    Koltuk * offset= ( Koltuk*)parameter;
     printf("\tK2<%d>:T%d\n ",clock(),offset->thread_no);
     if(trasK.dolumu==1)
-       printf("girildi");
+       printf("girildi\n");wait(TrasKoltugu);
     offset->dolumu=0;
     trasK.thread_no=offset->thread_no;
     mevcutSayisi--;
     pthread_create(&trasid,NULL,TrasKoltugu,&trasK);
     pthread_join(trasid,0);
+   // pthread_mutex_lock(&tras_Koltugu_Mutex);
      sem_post(&bekleme_Koltugu2_Mutex);                           //semaphore kritik bolgesi Bitisi
 
 }
@@ -61,31 +63,33 @@ void * BeklemeKoltugu3(void * parameter ){
     Koltuk * offset= ( Koltuk*)parameter;
     printf("\tK3<%d>:T%d\n",clock(),offset->thread_no);
     if(trasK.dolumu==1)
-       printf("girildi");
+       printf("girildi\n");wait(TrasKoltugu);
     offset->dolumu=0;
     trasK.thread_no=offset->thread_no;
     mevcutSayisi--;
     pthread_create(&trasid,NULL,TrasKoltugu,&trasK);
     pthread_join(trasid,0);
+  //  pthread_mutex_lock(&tras_Koltugu_Mutex);
      sem_post(&bekleme_Koltugu3_Mutex);                           //semaphore kritik bolgesi Bitisi
 
 }
 
 void * TrasKoltugu(void * parameter ){
     printf("80.satir\n");
-    sem_wait(&tras_Koltugu_Mutex);                           //semaphore kritik bolgesi baslangici
+   // sem_wait(&tras_Koltugu_Mutex);                           //semaphore kritik bolgesi baslangici
+    pthread_mutex_lock(&tras_Koltugu_Mutex);
     trasK.dolumu=1;
     printf("\tK0<%d>:T%d\n",clock(),trasK.thread_no);
     sleep(trasK.thread_no);
     trasK.dolumu=0;
-    sem_post(&tras_Koltugu_Mutex);                           //semaphore kritik bolgesi Bitisi
+    pthread_mutex_unlock(&tras_Koltugu_Mutex);
+   // sem_post(&tras_Koltugu_Mutex);                           //semaphore kritik bolgesi Bitisi
 }
 
 void *BerberSalonu(int tid){
     int i=0;
   //  sem_wait(&semaMutex);                           //semaphore kritik bolgesi baslangici
-    mevcutSayisi++;
-    printf("%d mevcut sayisi\n",mevcutSayisi);
+    mevcutSayisi++;sleep(tid);
     //printf("\t\t%d. Thread Kiritik Bolgede\n",tid);
         if(Beklemekoltugu[0].dolumu==0){
             printf("91.satir %d\n",tid);
@@ -93,24 +97,27 @@ void *BerberSalonu(int tid){
             Beklemekoltugu[0].thread_no=tid;
             pthread_create(tids+tid,NULL,BeklemeKoltugu1,&Beklemekoltugu[0]);
            i= pthread_join(tids[tid], 0);
+         //  pthread_mutex_lock(&tras_Koltugu_Mutex);
         }else if(Beklemekoltugu[1].dolumu==0){
             printf("97.satir %d\n",tid);
             Beklemekoltugu[1].dolumu=1;
             Beklemekoltugu[1].thread_no=tid;
              pthread_create(tids+tid,NULL,BeklemeKoltugu2,&Beklemekoltugu[1]);
             i=pthread_join(tids[tid], 0);
+           // pthread_mutex_lock(&tras_Koltugu_Mutex);
         }else if(Beklemekoltugu[2].dolumu==0){
             printf("103.satir %d\n",tid);
             Beklemekoltugu[2].dolumu=1;
             Beklemekoltugu[2].thread_no=tid;
             pthread_create(tids+tid,NULL,BeklemeKoltugu3,&Beklemekoltugu[2]);
             i=pthread_join(tids[tid], 0);
+           // pthread_mutex_lock(&tras_Koltugu_Mutex);
         }
         if(mevcutSayisi==4){
                 printf("Girin sayisi 4 oldu beklenyor \n");
-                sleep(10);
+               // sleep(10);
          }
-
+printf("%d mevcut sayisi\n",mevcutSayisi);
 
    // sem_post(&semaMutex);                           //semaphore kritik bolgesi Bitisi
 
@@ -133,7 +140,7 @@ int main(){
             printf("\n mutex init hatasi\n");
             return 1;
             }
-    if (sem_init(&tras_Koltugu_Mutex, 0, 1) != 0)
+    if (pthread_mutex_init(&tras_Koltugu_Mutex, NULL) != 0)
              {
             printf("\n mutex init hatasi\n");
             return 1;
@@ -144,10 +151,10 @@ int main(){
           return 1;
       }
 
-
     for (int var = 1; var <= 10; ++var) {
           pthread_create(tids+var, NULL, BerberSalonu, var);
     }
+   // pthread_mutex_lock(&tras_Koltugu_Mutex);
 
     for (int var = 1; var <= 10; ++var) {
          pthread_join(tids[var], 0);
@@ -159,7 +166,7 @@ int main(){
     sem_destroy(&bekleme_Koltugu1_Mutex);
     sem_destroy(&bekleme_Koltugu2_Mutex);
     sem_destroy(&bekleme_Koltugu3_Mutex);
-    sem_destroy(&tras_Koltugu_Mutex);
+    pthread_mutex_destroy(&tras_Koltugu_Mutex);
 
 	
 	
